bd6842da81fd78a118baa078339c1d098d8479fa
Dora0105 <86813311+Dora0105@users.noreply.github.com>
2021-09-18 17:59:18 +0300

    Add files via upload

---
+++ сереализация десереализация.cpp
@@ -0,0 +1,41 @@
+﻿// сереализация десереализация.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+template<class T>
+class A
+{
+private:
+	int size;
+	T* array = nullptr;
+public:
+	A(int size1, T* array1) :size(size1)
+	{
+		array = new T[size1];
+		for (int i = 0; i < size; i++) { array[i] = array1[i]; }
+	}
+	void write(const char* name)
+	{
+		ofstream fout(name);
+		fout << "<array numElem='" << size << "'>" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			fout << "<item>" << array[i] << "</item>" << endl;
+		}
+		fout.close();
+	}
+	~A()
+	{
+		delete[] array;
+	}
+};
+
+int main()
+{
+	int arr[3];
+	arr[0] = 1; arr[1] = 2; arr[2] = 3;
+	A<int> example(3, arr);
+	example.write("hello.txt");
+}
